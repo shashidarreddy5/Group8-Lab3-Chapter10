4d9b8b9cfc0f994dae5486963dac37c8b4be9975
vinithareddy5 <59577221+vinithareddy5@users.noreply.github.com>
2020-11-24 00:23:41 -0500

    commit by vinitha to create mouseenter handler

--- ch10-project2-start-master/js/project2.js
+++ ch10-project2-start-master/js/project2.js
@@ -5,6 +5,30 @@ $(document).ready(function(){
     for(i=0;i<= images.length-1; i++) 
         image[0].innerHTML += `<img src="./images/square/${images[i].path}" alt="${images[i].title}"/>`;
     
+        /* Code by Vinitha to create mouseenter handler */
+           
+        $("img").mouseenter(function(event){
+            $(this).addClass("gray");
+            var src=$(this).attr("src");
+            var alt=$(this).attr("alt");
+            var div = $("<div></div>").attr('id','preview');
+            var img = $("<img></img>").attr('src',src.replace('square','medium')).attr('alt',alt);
+            div.append(img);
+            images.forEach(ele => {
+                if (ele.title == $(this).attr('alt')) {
+                    var p = $("<p></p>").html(ele.title+"</br>"+ele.city+", "+ele.country+" ["+ele.taken+"]");
+                    div.append(p);
+                }
+            });
+            
+            $("body").append(div);
+
+            $("#preview").css("left",event.pageX+30);
+            $("#preview").css("top",event.pageY);
+            $("#preview").fadeIn(1000); 
+            
+        });
+ 
      
     /* Code by Shashidar reddy for mousemove and mouseleave events */
         $(".gallery img").mousemove(function(event){  
